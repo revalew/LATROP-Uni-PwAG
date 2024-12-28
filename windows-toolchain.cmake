befd54df106315d513bfbe2c99ca61ea77a68838
Maks <makskisiel@gmail.com>
2024-12-28 22:09:34 +0100

    .. (version 0.0.17)

--- windows-toolchain.cmake
+++ windows-toolchain.cmake
@@ -1,6 +1,11 @@
 set(CMAKE_SYSTEM_NAME Windows)
 set(CMAKE_SYSTEM_PROCESSOR x86_64)
 
+find_package(FreeGLUT REQUIRED)
+if(FreeGLUT_FOUND)
+  target_link_libraries(${EXE_NAME} PRIVATE FreeGLUT::freeglut)
+endif()
+
 # Compiler setup
 set(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc)
 set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
